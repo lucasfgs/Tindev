59c1c335c11b6ad6c40bd446eb9403007215c17b
unknown <blooodcf@gmail.com>
2019-08-07 15:41:01 -0300

    Project structure

---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import logo from "../../assets/logo.svg";
+
+const Login = () => (
+  <div>
+    <img src={logo} alt="Tindev" />
+  </div>
+);
+
+export default Login;
